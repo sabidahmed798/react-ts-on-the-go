b02412c04b2e3578d9fa20798357e93d62debbbf
sabidwebdeveloper798 <sabidahmed798@gmail.com>
2026-09-05 00:14:42 +0600

    Update code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { Suspense } from "react";
 import "./App.css";
+import type { CountryType } from "./components/type";
+import Countries from "./components/Countries";
+
+//step 1: create a promise to load data
+const countriesPromise = async (): Promise<CountryType[]> => {
+  const res = await fetch("https://openapi.programming-hero.com/api/all");
+  const data = await res.json();
+  return data.countries;
+};
 
 function App() {
   return (
     <>
       <h2>World on the Go....</h2>
+      <Suspense fallback={<div>Loading</div>}>
+        <Countries countriesPromise={countriesPromise()}></Countries>
+      </Suspense>
     </>
   );
 }
---
+++ src/components/Countries.tsx
@@ -0,0 +1,16 @@
+import { use } from "react";
+import type { CountryType } from "./type";
+
+export interface CountriesProps {
+  countriesPromise: Promise<CountryType[]>;
+}
+
+export default function Countries({ countriesPromise }: CountriesProps) {
+  const countries = use(countriesPromise);
+  console.log(countries);
+  return (
+    <div>
+      <h2>Countries:</h2>
+    </div>
+  );
+}
